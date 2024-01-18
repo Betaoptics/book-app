4f6f712ee05c5b3b54aeaf3c463fd28e8b1e247a
betaoptics <niinimaki23@hotmail.com>
2024-01-18 16:25:49 +0200

    Hotfix #3, Added Guest to MongoDB

--- src/Backend/config/dbConn.js
+++ src/Backend/config/dbConn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // console.log(process.env.DATABASE_URL);
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect(process.env.DATABASE_URL || process.env.GUEST_URL);
     } catch (err) {
         console.log(err);
     }
